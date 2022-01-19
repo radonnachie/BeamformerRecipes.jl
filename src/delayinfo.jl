cab621662f6dbfe7a39c522faf0ff98f2c4d11bc
David MacMahon <davidm@astro.berkeley.edu>
2022-01-19 08:08:17 +0200

    Add jds and dut1 fields to DelayInfo

--- src/delayinfo.jl
+++ src/delayinfo.jl
@@ -4,6 +4,8 @@ Base.Base.@kwdef mutable struct DelayInfo <: Info
     delays::AbstractArray{Float64,3} = Array{Float64,3}(undef, 0, 0, 0)
     rates::AbstractArray{Float64,3} = Array{Float64,3}(undef, 0, 0, 0)
     time_array::AbstractVector{Float64} = Vector{Float64}(undef, 0)
+    jds::AbstractVector{Float64} = Vector{Float64}(undef, 0)
+    dut1::Union{Float64,Nothing} = nothing
 end # mutable struct DelayInfo
 
 #=
